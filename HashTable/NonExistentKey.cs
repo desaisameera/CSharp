3abb1fc740ecd44f29d9d5542623b1aa2d345462
sameera <ssd9035@rit.edu>
2014-09-07 22:45:04 -0400

    Added an exception class for non-existent key

---
+++ HashTable/NonExistentKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RIT_CS {
+    /// <summary>
+    /// An exception used to indicate a problem with how
+    /// a HashTable instance is being accessed
+    /// </summary>
+    public class NonExistentKey<Key> : Exception {
+        /// <summary>
+        /// The key that caused this exception to be raised
+        /// </summary>
+        public Key BadKey { get; private set; }
+
+        /// <summary>
+        /// Create a new instance and save the key that
+        /// caused the problem.
+        /// </summary>
+        /// <param name="k">
+        /// The key that was not found in the hash table
+        /// </param>
+        public NonExistentKey(Key k) :
+            base("Non existent key in HashTable: " + k) {
+            BadKey = k;
+        }
+    }
+}
+
