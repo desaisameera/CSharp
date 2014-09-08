cefb09e54efb3c66e4a543d09a6573b26bcea7e0
sameera <ssd9035@rit.edu>
2014-09-07 22:45:53 -0400

    Added class TableFactory

---
+++ HashTable/TableFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RIT_CS {
+    class TableFactory {
+        /// <summary>
+        /// Create a Table.
+        /// </summary>
+        /// <param name="K">the key type</param>
+        /// <param name="V">the value type</param>
+        /// <param name="capacity">The initial maximum size of the table</param>
+        /// <param name="loadThreshold">
+        /// The fraction of the table's capacity that when
+        /// reached will cause a rebuild of the table to a 50% larger size
+        /// </param>
+        /// <returns>A new instance of Table</returns>
+        public static Table<K, V> Make<K, V>(int capacity = 100, double loadThreshold = 0.75) {
+            LinkedHashTable<K, V> table = new LinkedHashTable<K, V>(capacity, loadThreshold);
+            return table;
+        }
+    }
+}
+
