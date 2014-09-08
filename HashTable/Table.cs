6b1d68ef13c21146a9d8280acabb0643fc15e97f
sameera <ssd9035@rit.edu>
2014-09-07 22:48:19 -0400

    Class for table

---
+++ HashTable/Table.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RIT_CS {
+    /// <summary>
+    /// An associative (key-value) data structure.
+    /// A given key may not appear more than once in the table,
+    /// but multiple keys may have the same value associated with them.
+    /// Tables are assumed to be of limited size are expected to automatically
+    /// expand if too many entries are put in them.
+    /// </summary>
+    /// <param name="Key">the types of the table's keys (uses Equals())</param>
+    /// <param name="Value">the types of the table's values</param>
+
+    interface Table<Key, Value> : IEnumerable<Key> {
+        /// <summary>
+        /// Add a new entry in the hash table. If an entry with the
+        /// given key already exists, it is replaced without error.
+        /// put() always succeeds.
+        /// (Details left to implementing classes.)
+        /// </summary>
+        /// <param name="k">the key for the new or existing entry</param>
+        /// <param name="v">the (new) value for the key</param>
+        void Put(Key k, Value v);
+
+        /// <summary>
+        /// Does an entry with the given key exist?
+        /// </summary>
+        /// <param name="k">the key being sought</param>
+        /// <returns>true iff the key exists in the table</returns>
+        bool Contains(Key k);
+
+        /// <summary>
+        /// Fetch the value associated with the given key.
+        /// </summary>
+        /// <param name="k">The key to be looked up in the table</param>
+        /// <returns>the value associated with the given key</returns>
+        /// <exception cref="NonExistentKey">if Contains(key) is false</exception>
+        Value Get(Key k);
+    }
+}
+
